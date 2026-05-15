c1e25d19b55c40ad7c7d82ed0241086701655c2e
Jacquardo <sedrajacquardo@gmail.com>
2026-05-15 14:20:56 +0300

    Update image paths to remove leading './'

--- js/config/backgrounds.js
+++ js/config/backgrounds.js
@@ -8,62 +8,62 @@ export const NOTE_BACKGROUNDS = [
   {
     id: 'img1',
     name: 'Fond 1',
-    thumbnail: './assets/img1.png',
-    value: './assets/img1.png'
+    thumbnail: 'assets/img1.png',
+    value: 'assets/img1.png'
   },
   {
     id: 'img2',
     name: 'Fond 2',
-    thumbnail: './assets/img2.png',
-    value: './assets/img2.png'
+    thumbnail: 'assets/img2.png',
+    value: 'assets/img2.png'
   },
   {
     id: 'img3',
     name: 'Fond 3',
-    thumbnail: './assets/img3.png',
-    value: './assets/img3.png'
+    thumbnail: 'assets/img3.png',
+    value: 'assets/img3.png'
   },
   {
     id: 'img4',
     name: 'Fond 4',
-    thumbnail: './assets/img4.png',
-    value: './assets/img4.png'
+    thumbnail: 'assets/img4.png',
+    value: 'assets/img4.png'
   },
   {
     id: 'img5',
     name: 'Fond 5',
-    thumbnail: './assets/img5.png',
-    value: './assets/img5.png'
+    thumbnail: 'assets/img5.png',
+    value: 'assets/img5.png'
   },
   {
     id: 'img6',
     name: 'Fond 6',
-    thumbnail: './assets/img6.png',
-    value: './assets/img6.png'
+    thumbnail: 'assets/img6.png',
+    value: 'assets/img6.png'
   },
   {
     id: 'img7',
     name: 'Fond 7',
-    thumbnail: './assets/img7.png',
-    value: './assets/img7.png'
+    thumbnail: 'assets/img7.png',
+    value: 'assets/img7.png'
   },
   {
     id: 'img8',
     name: 'Fond 8',
-    thumbnail: './assets/img8.png',
-    value: './assets/img8.png'
+    thumbnail: 'assets/img8.png',
+    value: 'assets/img8.png'
   },
   {
     id: 'img9',
     name: 'Fond 9',
-    thumbnail: './assets/img9.png',
-    value: './assets/img9.png'
+    thumbnail: 'assets/img9.png',
+    value: 'assets/img9.png'
   },
   {
     id: 'img10',
     name: 'Fond 10',
-    thumbnail: './assets/img10.png',
-    value: './assets/img10.png'
+    thumbnail: 'assets/img10.png',
+    value: 'assets/img10.png'
   }
 ];
 
