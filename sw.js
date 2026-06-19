e3e7f4e9882e40385309aeed401a75235530700c
Jacquardo <sedrajacquardo@gmail.com>
2026-06-19 14:26:59 +0300

    Increment CACHE_VERSION to v11.0.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v11.0.7';   // ← incrémenté (responsive.css, components.css, layout.css mis à jour)
+const CACHE_VERSION = 'v11.0.8';   // ← incrémenté (responsive.css, components.css, layout.css mis à jour)
 
 const CACHE_NAME = `notes-me-${CACHE_VERSION}`;
 
